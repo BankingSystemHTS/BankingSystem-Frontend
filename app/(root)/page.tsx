c66895b64e176b53837286de459354d351ee064b
nathan <000savior001@gmail.com>
2024-11-12 19:14:03 -0800

    RightSidebar profile image, user info added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,29 @@
 import HeaderBox from '@/components/ui/header'
+import RightSidebar from '@/components/ui/RightSidebar';
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: "Nathan" };
+  const loggedIn =
+  {
+    firstName: "Nathan", 
+    lastName: "Chan",
+    email: 'nathan109@gmail.com'
+
+
+  };
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox 
+          <HeaderBox
             type="greeting"
             title="Welcome, "
-            user= {loggedIn?.firstName || "Guest"}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access & Manage your account 
             and trasactions efficiently."
           />
-          <TotalBalanceBox 
+          <TotalBalanceBox
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={12}
@@ -23,6 +31,11 @@ const Home = () => {
         </header>
         Recent Transations
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}
+      />
     </section>
   )
 }
--- components/ui/RightSidebar.tsx
+++ components/ui/RightSidebar.tsx
@@ -1,10 +1,58 @@
+
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-const RightSideBar = () => {
+const RightSideBar = ({ user, transactions, banks }: RightSidebarProps) => {
   return (
-    <div>
-      
-    </div>
+   // hidden by default, show when xl:flex
+    <aside className="right-sidebar">
+      <section className="flex flex-col pb-8">
+         <div className="profile-banner"/>
+         <div className="profile"/>
+         <div className="profile-img">
+            <span className="text-4xl font-bold
+            text-blue-500">{user.firstName[0]}</span>
+         </div>
+
+         <div className="profile-details">
+            <h1 className="profile-name">
+               {user.firstName} {user.lastName}
+            </h1>
+            <p className="profile-email">
+               {user.email}
+            </p>
+         </div>
+      </section>
+
+      <section className="banks">
+         <div className="flex w-full justify-between">
+            <h2 className="header-2">My Banks</h2>
+            <Link href="/" className="flex gap-2">
+               <Image 
+                  src="/icons/plus.svg"
+                  width={20}
+                  height={20}
+                  alt="plus"
+               />
+               <h2 className="text-slate-500 text-sm font-regular">Add Bank</h2>
+            </Link>
+         </div>
+         {/* {banks?.length > 0 && (
+            <div className="relative flex flex-1 flex-col 
+            items-center justify-center gap-5">
+               <div className='relative '>
+                  BANK CARD 1
+               </div>
+               {banks[1] && (
+                  <div >
+                     BANK CARD 2
+                  </div>
+               )}
+            </div>
+         )} */}
+      </section>
+    </aside>
   )
 }
 
