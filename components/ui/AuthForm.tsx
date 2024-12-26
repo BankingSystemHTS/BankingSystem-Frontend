ebe2c9c3ec12adef1382c871fc1a836382b8c51c
nathan <000savior001@gmail.com>
2024-12-26 09:33:21 -0800

    Sign in authform footer added

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -118,17 +118,31 @@ const AuthForm = ({ type }: { type: string }) => {
                 placeholder="Enter your email"
                 label='Email'
               />
+              <div className="flex flex-col gap-4">
+                <Button type="submit" className="form-btn" disabled={isLoading}>
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+                </Button>
+              </div>
 
-              <Button type="submit" className="form-btn" disabled={isLoading}>
-                {isLoading ? (
-                  <>
-                    <Loader2 size={20} className="animate-spin" /> &nbsp;
-                    Loading...
-                  </>
-                ) : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
-              </Button>
             </form>
           </Form>
+
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === 'sign-in'
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link href={type === 'sign-in' ? '/sign-up'
+              : "/sign-in"} className="form-link">
+              {type == 'sign-in' ? 'Sign up' : 'Sign in'}
+            </Link>
+          </footer>
         </>
       )}
     </section>
